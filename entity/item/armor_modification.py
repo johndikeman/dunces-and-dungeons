e33632d4b2b28da9050608a4f0e3d976d9c39733
ThePotatoV2 <eggerc@HSCompSci16.needville.isd.esc4.net>
2015-10-28 12:35:08 -0500

    renamed item_mod to weapon_mod. wrote armor_mod

--- entity/item/armor_modification.py
+++ entity/item/armor_modification.py
@@ -24,89 +24,89 @@ class Common(ModifyItems):
 
 class Basic(Common):
 	def __init__(self,item):
-		item.pow*=1
+		item.armor*=1
 
 	def do_turn(self,target,damage):
-		target.take_damage(damage)
+		return damage
 
 	def to_str(self):
 		return "Basic"
 
 class Iron(Common):
 	def __init__(self,item):
-		item.pow*=1.1
+		item.armor*=1.1
 
 	def do_turn(self,target,damage):
 		chance=base.D20.roll()
 		if chance>19:
 			target.statuses.append(s.Maim(1,1))
-		target.take_damage(damage)
+		return damage
 
 	def to_str(self):
 		return "Iron"
 
 class Rusty(Common):
 	def __init__(self,item):
-		item.pow*=.5
+		item.armor*=.5
 
 	def do_turn(self,target,damage):
 		chance=base.D20.roll()
 		if chance>19:
 			target.statuses.append(s.Poison(2,2*self.level))
-		target.take_damage(damage)
+		return damage
 
 	def to_str(self):
 		return "Rusty"
 
 class Used(Common):
 	def __init__(self,item):
-		item.pow*=.8
+		item.armor*=.8
 
 	def do_turn(self,target,damage):
-		target.take_damage(damage)
+		return damage
 
 	def to_str(self):
 		return "Used"
 
 class Weathered(Common):
 	def __init__(self,item):
-		item.pow*=.7
+		item.armor*=.7
 
 	def do_turn(self,target,damage):
-		target.take_damage(damage)
+		return damage
 
 	def to_str(self):
 		return "Weathered"
 
 class Wooden(Common):
 	def __init__(self,item):
-		item.pow*=.5
+		item.armor*=.5
 
 	def do_turn(self,target,damage):
-		target.take_damage(damage)
+		return damage
 
 	def to_str(self):
 		return "Wooden"
 
 class Notched(Common):
 	def __init__(self,item):
-		item.pow*=.9
+		item.armor*=.9
 
 	def do_turn(self,target,damage):
 		chance=base.D20.roll()
 		if chance>19:
 			target.statuses.append(s.Bleeding(2,4*self.level))
-		target.take_damage(damage)
+		return damage
 
 	def to_str(self):
 		return "Notched"
 
 class Scratched(Common):
 	def __init__(self,item):
-		item.pow*=.9
+		item.armor*=.9
 
 	def do_turn(self,target,damage):
-		target.take_damage(damage)
+		return damage
 
 	def to_str(self):
 		return "Scratched"
@@ -123,27 +123,27 @@ class Uncommon(ModifyItems):
 
 class Good(Uncommon):
 	def __init__(self,item):
-		item.pow*=1.3
+		item.armor*=1.3
 
 	def do_turn(self,target,damage):
-		target.take_damage(damage)
+		return damage
 
 	def to_str(self):
 		return "Good"
 
 class Shining(Uncommon):
 	def __init__(self,item):
-		item.pow*=1.6
+		item.armor*=1.6
 
 	def do_turn(self,target,damage):
-		target.take_damage(damage)
+		return damage
 
 	def to_str(self):
 		return "Shining"
 
 class Steel(Uncommon):
 	def __init__(self,item):
-		item.pow*=1.3
+		item.armor*=1.3
 
 	def do_turn(self,target,damage):
 		chance = base.D20.roll()
@@ -152,27 +152,27 @@ class Steel(Uncommon):
 		chance2 = base.D20.roll()
 		if chance2 > 18:
 			target.statuses.append(s.Maim(2,2))
-		target.take_damage(damage)
+		return damage
 
 	def to_str(self):
 		return "Steel"
 
 class Archaic(Uncommon):
 	def __init__(self,item):
-		item.pow*=1.3
+		item.armor*=1.3
 
 	def do_turn(self,target,damage):
 		chance = base.D20.roll()
 		if chance > 16:
 			target.statuses.append(s.Poison(3,3*self.level))
-		target.take_damage(damage)
+		return damage
 
 	def to_str(self):
 		return "Archaic"
 
 class Brutal(Uncommon):
 	def __init__(self,item):
-		item.pow*=1.5
+		item.armor*=1.5
 
 	def do_turn(self,target,damage):
 		chance = base.D20.roll()
@@ -181,7 +181,7 @@ class Brutal(Uncommon):
 		chance2 = base.D20.roll()
 		if chance2 > 16:
 			target.statuses.append(s.Maim(1,3))
-		target.take_damage(damage)
+		return damage
 
 	def to_str(self):
 		return "Brutal"
@@ -198,50 +198,50 @@ class Rare(ModifyItems):
 
 class Ceremonial(Rare):
 	def __init__(self,item):
-		item.pow*=2.5
+		item.armor*=2.5
 
 	def do_turn(self,target,damage):
 		chance = base.D20.roll()
 		if chance > 16:
 			self.owner.owner.gold+=self.level
-		target.take_damage(damage)
+		return damage
 
 	def to_str(self):
 		return "Ceremonial"
 
 class Silver(Rare):
 	def __init__(self,item):
-		item.pow*=2.4
+		item.armor*=2.4
 
 	def do_turn(self,target,damage):
 		#not finished, make wolves weak to it.
-		target.take_damage(damage)
+		return damage
 
 	def to_str(self):
 		return "Silver"
 
 class Killing(Rare):
 	def __init__(self,item):
-		item.pow*=3
+		item.armor*=3
 
 	def do_turn(self,target,damage):
 		chance = base.D20.roll()
 		if chance > 16:
-			damage*=((chance-16)/2)+base.D20.roll()
-		target.take_damage(damage)
+			damage/=((chance-16)/2)+base.D20.roll()
+		return damage
 
 	def to_str(self):
 		return "Killing"
 
 class Blessed(Rare):
 	def __init__(self,item):
-		item.pow*=3.2
+		item.armor*=3.2
 
 	def do_turn(self,target,damage):
 		chance = base.D20.roll()
 		if chance > 18:
 			target.statuses.append(s.Blind(math.round(math.pow(1.1,self.level))))
-		target.take_damage(damage)
+		return damage
 
 	def to_str(self):
 		return "Blessed"
@@ -258,45 +258,45 @@ class Legendary(ModifyItems):
 
 class Kingly(Legendary):
 	def __init__(self,item):
-		item.pow*=4
+		item.armor*=4
 
 	def do_turn(self,target,damage):
 		chance = base.D20.roll()
 		if chance > 16:
 			self.owner.owner.gold+=self.level*1.5
-		target.take_damage(damage)
+		return damage
 
 	def to_str(self):
 		return "Kingly"
 
 class Enchanted(Legendary):
 	def __init__(self,item):
-		item.pow*=4.2
+		item.armor*=4.2
 
 	def do_turn(self,target,damage):
 		chance = base.D20.roll()
 		if chance > 18:
 			target.statuses.append(s.Sleep())
-		target.take_damage(damage)
+		return damage
 
 	def to_str(self):
 		return "Enchanted"
 
 class Master(Legendary):
 	def __init__(self,item):
-		item.pow*=4.6
+		item.armor*=4.6
 
 	def do_turn(self,target,damage):
 		chance = base.D20.roll()
 		if chance > 16:
-			damage *= 2.5
+			damage /= 2.5
 		chance2 = base.D20.roll()
 		if chance2 > 16:
 			target.statuses.append(s.Maim(2))
 		chance3 = base.D20.roll()
 		if chance3 > 16:
 			target.statuses.append(s.Bleeding(2,self.level*6))
-		target.take_damage(damage)
+		return damage
 
 	def to_str(self):
 		return "Master"
@@ -313,7 +313,7 @@ class Divined(ModifyItems):
 
 class Celestial(Divined):
 	def __init__(self,item):
-		item.pow*=6
+		item.armor*=6
 
 	def do_turn(self,target,damage):
 		chance=base.D40.roll()
@@ -321,30 +321,31 @@ class Celestial(Divined):
 			print "A blinding light flashes from the Heavens and blinds and reveals all enemies in the room!"
 			print 'Hey if there is an error its on 323 of item_modification.py'
 			for a in self.owner.owner.party.current_dungeon.active_room.things:
-				a.statuses.append(s.Blind(6))
-		target.take_damage(damage)
+				if isinstance(a,Monster):
+					a.statuses.append(s.Blind(6))
+		return damage
 
 	def to_str(self):
 		return "Celestial"
 
 class Divine(Divined):
 	def __init__(self,item):
-		item.pow*=6.8
+		item.armor*=6.8
 
 	def do_turn(self,target,damage):
 		chance = base.D40.roll()
 		if chance > 39:
-			print 'Power surges through your body giving you increased damage and speed!'
-			self.owner.owner.action_points+=3
-			damage*=4
-		target.take_damage(damage)
+			print 'Power surges through your body giving you increased armor and health!'
+			self.owner.owner.health*=3
+			self.damage/=4
+		return damage
 
 	def to_str(self):
 		return "Divine"
 
 class Heavenly(Divined):
 	def __init__(self,item):
-		item.pow*=6.4
+		item.armor*=6.4
 
 	def do_turn(self,target,damage):
 		chance = base.D40.roll()
@@ -352,24 +353,24 @@ class Heavenly(Divined):
 			print 'Heavenly Fire leaps from the body of your enemy, striking all enemies in the room'
 			print 'Hey if there is an error its on 354 of item_modification.py'
 			for a in self.owner.owner.party.current_dungeon.active_room.things:
-				a.take_damage(20*self.level)
-				chance2=base.D20.roll()
-				if chance2>14:
-					a.statuses.append(s.Burn(self.level,self.level*8))
-		target.take_damage(damage)
+				if isinstance(a,Monster):
+					a.take_damage(self.owner,20*self.level)
+					chance2=base.D20.roll()
+					if chance2>14:
+						a.statuses.append(s.Burn(self.level,self.level*8))
 
 	def to_str(self):
 		return "Heavenly"
 
 class Arch(Divined):
 	def __init__(self,item):
-		item.pow*=8
+		item.armor*=8
 
 	def do_turn(self,target,damage):
 		chance=base.D100.roll()
 		if chance >99:
 			print 'The Arch Powers of the World infuse your body!'
-			print 'Mortal Strike!'
+			print 'Mortal Blast!'
 			target.statuses.append(s.Burn(self.level,100))
 			target.statuses.append(s.Poison(self.level,120))
 			target.statuses.append(s.Bleeding(self.level,1000))
@@ -379,8 +380,7 @@ class Arch(Divined):
 			target.statuses.append(s.Blind(self.level*2))
 			for a in self.owner.owner.party.inventory:
 				a.statuses.append(s.Healing())
-			damage*=10
-		target.take_damage(damage)
+			damage/=10
 
 	def to_str(self):
 		return "Arch"
---
+++ entity/item/weapon_modification.py
@@ -0,0 +1,388 @@
+import base, random, time
+import entity.status.player_statuses as s
+import math
+
+class ModifyItems(object):
+	def __init__(self):
+		pass
+
+	def do_turn(self,target,damage):
+		pass
+
+	def to_str(self):
+		return "Modified"
+
+class Common(ModifyItems):
+	def __init__(self):
+		pass
+
+	def do_turn(self,target,damage):
+		pass
+
+	def to_str(self):
+		return "Common"
+
+class Basic(Common):
+	def __init__(self,item):
+		item.pow*=1
+
+	def do_turn(self,target,damage):
+		target.take_damage(self.owner,damage)
+
+	def to_str(self):
+		return "Basic"
+
+class Iron(Common):
+	def __init__(self,item):
+		item.pow*=1.1
+
+	def do_turn(self,target,damage):
+		chance=base.D20.roll()
+		if chance>19:
+			target.statuses.append(s.Maim(1,1))
+		target.take_damage(self.owner,damage)
+
+	def to_str(self):
+		return "Iron"
+
+class Rusty(Common):
+	def __init__(self,item):
+		item.pow*=.5
+
+	def do_turn(self,target,damage):
+		chance=base.D20.roll()
+		if chance>19:
+			target.statuses.append(s.Poison(2,2*self.level))
+		target.take_damage(self.owner,damage)
+
+	def to_str(self):
+		return "Rusty"
+
+class Used(Common):
+	def __init__(self,item):
+		item.pow*=.8
+
+	def do_turn(self,target,damage):
+		target.take_damage(self.owner,damage)
+
+	def to_str(self):
+		return "Used"
+
+class Weathered(Common):
+	def __init__(self,item):
+		item.pow*=.7
+
+	def do_turn(self,target,damage):
+		target.take_damage(self.owner,damage)
+
+	def to_str(self):
+		return "Weathered"
+
+class Wooden(Common):
+	def __init__(self,item):
+		item.pow*=.5
+
+	def do_turn(self,target,damage):
+		target.take_damage(self.owner,damage)
+
+	def to_str(self):
+		return "Wooden"
+
+class Notched(Common):
+	def __init__(self,item):
+		item.pow*=.9
+
+	def do_turn(self,target,damage):
+		chance=base.D20.roll()
+		if chance>19:
+			target.statuses.append(s.Bleeding(2,4*self.level))
+		target.take_damage(self.owner,damage)
+
+	def to_str(self):
+		return "Notched"
+
+class Scratched(Common):
+	def __init__(self,item):
+		item.pow*=.9
+
+	def do_turn(self,target,damage):
+		target.take_damage(self.owner,damage)
+
+	def to_str(self):
+		return "Scratched"
+
+class Uncommon(ModifyItems):
+	def __init__(self,item):
+		pass
+
+	def do_turn(self,target,damage):
+		pass
+
+	def to_str(self):
+		return "Uncommon"
+
+class Good(Uncommon):
+	def __init__(self,item):
+		item.pow*=1.3
+
+	def do_turn(self,target,damage):
+		target.take_damage(self.owner,damage)
+
+	def to_str(self):
+		return "Good"
+
+class Shining(Uncommon):
+	def __init__(self,item):
+		item.pow*=1.6
+
+	def do_turn(self,target,damage):
+		target.take_damage(self.owner,damage)
+
+	def to_str(self):
+		return "Shining"
+
+class Steel(Uncommon):
+	def __init__(self,item):
+		item.pow*=1.3
+
+	def do_turn(self,target,damage):
+		chance = base.D20.roll()
+		if chance > 16:
+			target.statuses.append(s.Bleeding(3,3*self.level))
+		chance2 = base.D20.roll()
+		if chance2 > 18:
+			target.statuses.append(s.Maim(2,2))
+		target.take_damage(self.owner,damage)
+
+	def to_str(self):
+		return "Steel"
+
+class Archaic(Uncommon):
+	def __init__(self,item):
+		item.pow*=1.3
+
+	def do_turn(self,target,damage):
+		chance = base.D20.roll()
+		if chance > 16:
+			target.statuses.append(s.Poison(3,3*self.level))
+		target.take_damage(self.owner,damage)
+
+	def to_str(self):
+		return "Archaic"
+
+class Brutal(Uncommon):
+	def __init__(self,item):
+		item.pow*=1.5
+
+	def do_turn(self,target,damage):
+		chance = base.D20.roll()
+		if chance > 16:
+			target.statuses.append(s.Bleeding(3,3*self.level))
+		chance2 = base.D20.roll()
+		if chance2 > 16:
+			target.statuses.append(s.Maim(1,3))
+		target.take_damage(self.owner,damage)
+
+	def to_str(self):
+		return "Brutal"
+
+class Rare(ModifyItems):
+	def __init__(self,item):
+		pass
+
+	def do_turn(self,target,damage):
+		pass
+
+	def to_str(self):
+		return "Rare"
+
+class Ceremonial(Rare):
+	def __init__(self,item):
+		item.pow*=2.5
+
+	def do_turn(self,target,damage):
+		chance = base.D20.roll()
+		if chance > 16:
+			self.owner.owner.gold+=self.level
+		target.take_damage(self.owner,damage)
+
+	def to_str(self):
+		return "Ceremonial"
+
+class Silver(Rare):
+	def __init__(self,item):
+		item.pow*=2.4
+
+	def do_turn(self,target,damage):
+		#not finished, make wolves weak to it.
+		target.take_damage(self.owner,damage)
+
+	def to_str(self):
+		return "Silver"
+
+class Killing(Rare):
+	def __init__(self,item):
+		item.pow*=3
+
+	def do_turn(self,target,damage):
+		chance = base.D20.roll()
+		if chance > 16:
+			damage*=((chance-16)/2)+base.D20.roll()
+		target.take_damage(self.owner,damage)
+
+	def to_str(self):
+		return "Killing"
+
+class Blessed(Rare):
+	def __init__(self,item):
+		item.pow*=3.2
+
+	def do_turn(self,target,damage):
+		chance = base.D20.roll()
+		if chance > 18:
+			target.statuses.append(s.Blind(math.round(math.pow(1.1,self.level))))
+		target.take_damage(self.owner,damage)
+
+	def to_str(self):
+		return "Blessed"
+
+class Legendary(ModifyItems):
+	def __init__(self,item):
+		pass
+
+	def do_turn(self,target,damage):
+		pass
+
+	def to_str(self):
+		return "Legendary"
+
+class Kingly(Legendary):
+	def __init__(self,item):
+		item.pow*=4
+
+	def do_turn(self,target,damage):
+		chance = base.D20.roll()
+		if chance > 16:
+			self.owner.owner.gold+=self.level*1.5
+		target.take_damage(self.owner,damage)
+
+	def to_str(self):
+		return "Kingly"
+
+class Enchanted(Legendary):
+	def __init__(self,item):
+		item.pow*=4.2
+
+	def do_turn(self,target,damage):
+		chance = base.D20.roll()
+		if chance > 18:
+			target.statuses.append(s.Sleep())
+		target.take_damage(self.owner,damage)
+
+	def to_str(self):
+		return "Enchanted"
+
+class Master(Legendary):
+	def __init__(self,item):
+		item.pow*=4.6
+
+	def do_turn(self,target,damage):
+		chance = base.D20.roll()
+		if chance > 16:
+			damage *= 2.5
+		chance2 = base.D20.roll()
+		if chance2 > 16:
+			target.statuses.append(s.Maim(2))
+		chance3 = base.D20.roll()
+		if chance3 > 16:
+			target.statuses.append(s.Bleeding(2,self.level*6))
+		target.take_damage(self.owner,damage)
+
+	def to_str(self):
+		return "Master"
+
+class Divined(ModifyItems):
+	def __init__(self,item):
+		pass
+
+	def do_turn(self,target,damage):
+		pass
+
+	def to_str(self):
+		return "Divine"
+
+class Celestial(Divined):
+	def __init__(self,item):
+		item.pow*=6
+
+	def do_turn(self,target,damage):
+		chance=base.D40.roll()
+		if chance>39:
+			print "A blinding light flashes from the Heavens and blinds and reveals all enemies in the room!"
+			print 'Hey if there is an error its on 323 of item_modification.py'
+			for a in self.owner.owner.party.current_dungeon.active_room.things:
+				if isinstance(a,Monster):
+					a.statuses.append(s.Blind(6))
+		target.take_damage(self.owner,damage)
+
+	def to_str(self):
+		return "Celestial"
+
+class Divine(Divined):
+	def __init__(self,item):
+		item.pow*=6.8
+
+	def do_turn(self,target,damage):
+		chance = base.D40.roll()
+		if chance > 39:
+			print 'Power surges through your body giving you increased damage and speed!'
+			self.owner.owner.action_points+=3
+			damage*=4
+		target.take_damage(self.owner,damage)
+
+	def to_str(self):
+		return "Divine"
+
+class Heavenly(Divined):
+	def __init__(self,item):
+		item.pow*=6.4
+
+	def do_turn(self,target,damage):
+		chance = base.D40.roll()
+		if chance > 39:
+			print 'Heavenly Fire leaps from the body of your enemy, striking all enemies in the room'
+			print 'Hey if there is an error its on 354 of item_modification.py'
+			for a in self.owner.owner.party.current_dungeon.active_room.things:
+				if isinstance(a,Monster):
+					a.take_damage(self.owner,20*self.level)
+					chance2=base.D20.roll()
+					if chance2>14:
+						a.statuses.append(s.Burn(self.level,self.level*8))
+		target.take_damage(self.owner,damage)
+
+	def to_str(self):
+		return "Heavenly"
+
+class Arch(Divined):
+	def __init__(self,item):
+		item.pow*=8
+
+	def do_turn(self,target,damage):
+		chance=base.D100.roll()
+		if chance >99:
+			print 'The Arch Powers of the World infuse your body!'
+			print 'Mortal Strike!'
+			target.statuses.append(s.Burn(self.level,100))
+			target.statuses.append(s.Poison(self.level,120))
+			target.statuses.append(s.Bleeding(self.level,1000))
+			target.statuses.append(s.Maim(self.level))
+			target.statuses.append(s.Stun(self.level/2))
+			target.statuses.append(s.Sleep())
+			target.statuses.append(s.Blind(self.level*2))
+			for a in self.owner.owner.party.inventory:
+				a.statuses.append(s.Healing())
+			damage*=10
+		target.take_damage(self.owner,damage)
+
+	def to_str(self):
+		return "Arch"
